92358c1cf6681ca04ac7fe832129e7b05aab5ccd
Rosinery <92558971+Rosinery@users.noreply.github.com>
2023-08-08 15:21:12 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# ProjetoMeteoroPro
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,49 @@
+var fundoImg, fundo;
+var ufoImg, ufo;
+var fogueteImg, foguete;
+var coinImg, coin;
+var explosaoImg, explosao; 
+
+var coinGroup;
+var ufoGroup;
+
+var score = 0;
+var life = 3;
+
+var gameState = "play";
+
+function preload() {
+
+
+}
+
+
+function setup() {
+  createCanvas(800,800);
+
+}
+
+function draw() {
+  background(0);
+
+  drawSprites();
+  
+  
+}
+
+function spawnAliens() {
+  
+}
+
+function spawnCoins() {
+  
+ 
+}
+
+function removeCoins() {
+  
+}
+
+function removeLife() {
+  
+ }
